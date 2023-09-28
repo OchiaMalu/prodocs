8842b9f4c6281c4c36c082a0a4b9cbf720d0b785
ochiamalu <linzhehao1108@126.com>
2023-09-28 20:02:37 +0800

    feat: 修改样式

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,23 +1,22 @@
 export default {
     base: "/",
-    lang: "zh-CN",
-    title: "程序员",
-    description: "",
+    title: "ProDocs",
+    description: "一站式编程学习网站",
     lastUpdated: true,
     cleanUrls: true,
     appearance: 'dark',
     head: [
         [
-            'link', {rel: 'icon', href: ''}
+            'link', {rel: 'icon', href: '/favicon.ico'}
         ]
     ],
     themeConfig: {
-        logo: "",
+        logo: {light: '/favicon.ico', dark: '/favicon_dark.ico'},
         outline: 'deep',
         returnToTopLabel: '回到顶部',
         nav: [
             {text: '首页', link: '/'},
-            {text: '开始阅读', link: '/components/index.md'}
+            {text: '开始阅读', link: '/components/index.md'},
         ],
         sidebar: [
             {
@@ -42,13 +41,21 @@ export default {
             }
         ],
         footer: {
-            message: '用心去做高质量的编程学习内容网站，欢迎<a target="_blank" style="color: var(--vp-c-brand)" href="https://github.com/QSXLtangsan">star ⭐</a>让更多人发现！',
+            message: '用心去做高质量的编程学习内容网站，欢迎<a target="_blank" style="color: red" href="https://gitee.com/ochiamaluo/prodocs">star ⭐</a>让更多人发现！',
             copyright: 'MIT Licensed | Copyright © 2023-present OchiaMalu'
         },
-        socialLinks: [{
+        socialLinks: [
+            {
+                icon: {
+                    svg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#c81d23" d="M11.984 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12a12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.016 0zm6.09 5.333c.328 0 .593.266.592.593v1.482a.594.594 0 0 1-.593.592H9.777c-.982 0-1.778.796-1.778 1.778v5.63c0 .327.266.592.593.592h5.63c.982 0 1.778-.796 1.778-1.778v-.296a.593.593 0 0 0-.592-.593h-4.15a.592.592 0 0 1-.592-.592v-1.482a.593.593 0 0 1 .593-.592h6.815c.327 0 .593.265.593.592v3.408a4 4 0 0 1-4 4H5.926a.593.593 0 0 1-.593-.593V9.778a4.444 4.444 0 0 1 4.445-4.444h8.296Z"/></svg>'
+                },
+                link: 'https://gitee.com/ochiamaluo/prodocs',
+            },
+            {
             icon: 'github',
-            link: ''
-        }],
+                link: 'https://github.com/OchiaMalu/prodocs'
+            }
+        ],
         docFooter: {
             prev: '上一篇文章',
             next: '下一篇文章'
--- docs/index.md
+++ docs/index.md
@@ -1,11 +1,11 @@
 ---
 layout: home
-title: 程序员
+title: ProDocs
 titleTemplate: 一站式编程学习网站
 
 hero:
-  name: "程序员"
-  text: "一个编程爱好者"
+  name: ProDocs
+  text: Programming learning documents
   tagline: 一起探索编程的无限可能
   editLink: true
   lastUpdated: true
@@ -19,42 +19,42 @@ hero:
       link: /components/
     - theme: alt
       text: Github仓库
-      link: 
+      link: https://github.com/OchiaMalu/prodocs
 features:
   - icon:
-      src: /icon_js.svg
+      src: /icon/icon_java.svg
+    title: Java
+    details: Java是世界上最好的语言.c
+    link: /component/JavaScript
+    linkText: 开始阅读
+  - icon:
+      src: /icon/icon_spring.svg
+    title: Spring
+    details: Spring框架是一个开放源代码的J2EE应用程序框架
+    link: /component/JavaScript
+    linkText: 开始阅读
+  - icon:
+      src: /icon/icon_springboot.svg
+    title: SpringBoot
+    details: 简化Spring应用的初始搭建以及开发过程
+    link: /component/JavaScript
+    linkText: 开始阅读
+  - icon:
+      src: /icon/icon_html.svg
+    title: HTML/CSS
+    details: HTML是超文本标记语言
+    link: /component/JavaScript
+    linkText: 开始阅读
+  - icon:
+      src: /icon/icon_js.svg
     title: JavaScript
     details: JavaScript是一种解释性语言
     link: /component/JavaScript
     linkText: 开始阅读
   - icon:
-      src: /icon_js.svg
-    title: JavaScript
-    details: JavaScript是一种解释性语言
-    link: /component/JavaScript
-    linkText: 开始阅读
-  - icon:
-      src: /icon_js.svg
-    title: JavaScript
-    details: JavaScript是一种解释性语言
-    link: /component/JavaScript
-    linkText: 开始阅读
-  - icon:
-      src: /icon_js.svg
-    title: JavaScript
-    details: JavaScript是一种解释性语言
-    link: /component/JavaScript
-    linkText: 开始阅读
-  - icon:
-      src: /icon_js.svg
-    title: JavaScript
-    details: JavaScript是一种解释性语言
-    link: /component/JavaScript
-    linkText: 开始阅读
-  - icon:
-      src: /icon_js.svg
-    title: JavaScript
-    details: JavaScript是一种解释性语言
+      src: /icon/icon_ts.svg
+    title: TypeScript
+    details: 在JavaScript的基础上添加静态类型定义
     link: /component/JavaScript
     linkText: 开始阅读
 ---
