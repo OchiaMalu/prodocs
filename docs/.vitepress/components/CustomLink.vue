2039d845d737ac70bd133dd8538bbcca0864eee1
ochiamalu <linzhehao1108@126.com>
2023-09-29 14:36:27 +0800

    feat: 新增Grid、Customer组件

---
+++ docs/.vitepress/components/CustomLink.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import {Icon} from '@iconify/vue';
+import {computed} from "vue";
+import Grid from "./Grid.vue";
+
+interface ILink {
+  href: string
+  title: string
+}
+
+const {href, title} = defineProps<ILink>()
+
+const hrefSource = computed(() => {
+  if (/bilibili\.com/.test(href))
+    return 'bilibili'
+  if (/youtube\.com/.test(href))
+    return 'youtube'
+  if (/github\.com/.test(href))
+    return 'github'
+  if (/gitee\.com/.test(href))
+    return 'gitee'
+  return 'web'
+})
+
+const jump = () => {
+  window.open(href)
+}
+</script>
+
+<template>
+  <div
+      style="width: 100%;border: 1px solid #D4D4D8;border-radius: 0.5rem;padding: 16px;margin-top: 10px;margin-bottom: 10px"
+      class="main" @click="jump">
+    <span style="font-weight: 600;overflow: hidden;width: 90%;text-overflow: ellipsis;white-space: nowrap;"
+          class="title">
+      {{ title }}
+    </span>
+    <div style="justify-content: space-between;display: flex;">
+      <div style="margin-top: 20px">
+        <Icon icon="tabler:brand-bilibili" color="#47A7D9" class="icon" v-if="hrefSource == 'bilibili'"/>
+        <Icon icon="mdi:youtube" color="#FF0000" class="icon" v-else-if="hrefSource == 'youtube'"/>
+        <Icon icon="devicon:github" class="icon" v-else-if="hrefSource == 'github'"/>
+        <Icon icon="simple-icons:gitee" color="#D0101A" class="icon" v-else-if="hrefSource == 'gitee'"/>
+        <Icon icon="icon-park:add-web" class="icon" v-else/>
+        <span
+            style="margin-left: 5px;opacity: .75;font-weight: 500;font-size: .875rem;line-height: 27px;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;width: 100%;">
+          {{ href }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.main:hover {
+  cursor: pointer;
+
+  > .title {
+    color: #314ad7;
+  }
+}
+
+.icon {
+  width: 1.75rem;
+  height: 1.75rem;
+  float: left
+}
+</style>
---
+++ docs/.vitepress/components/Grid.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import {computed} from "vue";
+
+interface IGrid {
+  text: string
+  desc?: string
+  link: string
+}
+
+defineProps<{
+  navData: IGrid[]
+}>()
+</script>
+
+<template>
+
+  <div class="grid">
+    <section v-for="navItem in navData">
+      <a :href="navItem.link" rel="noreferrer" target="_blank" class="group">
+        <section class="flex">
+      <span class="text">
+        {{ navItem.text }}
+      </span>
+          <span class="desc">
+        {{ navItem.desc }}
+      </span>
+        </section>
+      </a>
+    </section>
+  </div>
+</template>
+
+<style>
+.grid {
+  gap: 12px;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-auto-rows: auto;
+  display: grid;
+}
+
+.flex {
+  line-height: 24px;
+  padding: 12px 24px;
+  border-style: solid;
+  border-radius: 8px;
+  border-color: var(--vp-c-border);
+  border-width: 1px;
+  flex-direction: column;
+  display: flex;
+  height: 100%;
+}
+
+.text {
+  --un-text-opacity: 1;
+  font-weight: 500;
+  color: rgba(75, 85, 99, var(--un-text-opacity));
+}
+
+.desc {
+  opacity: .5;
+  --un-text-opacity: 1;
+  color: rgba(55, 65, 81, var(--un-text-opacity));
+  font-size: .875rem;
+  line-height: 1.25rem;
+  min-height: 20px;
+  margin-bottom: auto;
+}
+
+.flex:hover {
+  background-color: #efefef;
+}
+</style>
