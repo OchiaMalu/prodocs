f8f220e05f1fc811176f889324c92f0bfc36c850
ochiamalu <linzhehao1108@126.com>
2023-10-03 19:11:13 +0800

    feat: 引入markdown-it-katex以渲染数学公式

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,105 @@
+import markdownItKatex from 'markdown-it-katex'
+
+const customElements = [
+    'math',
+    'maction',
+    'maligngroup',
+    'malignmark',
+    'menclose',
+    'merror',
+    'mfenced',
+    'mfrac',
+    'mi',
+    'mlongdiv',
+    'mmultiscripts',
+    'mn',
+    'mo',
+    'mover',
+    'mpadded',
+    'mphantom',
+    'mroot',
+    'mrow',
+    'ms',
+    'mscarries',
+    'mscarry',
+    'mscarries',
+    'msgroup',
+    'mstack',
+    'mlongdiv',
+    'msline',
+    'mstack',
+    'mspace',
+    'msqrt',
+    'msrow',
+    'mstack',
+    'mstack',
+    'mstyle',
+    'msub',
+    'msup',
+    'msubsup',
+    'mtable',
+    'mtd',
+    'mtext',
+    'mtr',
+    'munder',
+    'munderover',
+    'semantics',
+    'math',
+    'mi',
+    'mn',
+    'mo',
+    'ms',
+    'mspace',
+    'mtext',
+    'menclose',
+    'merror',
+    'mfenced',
+    'mfrac',
+    'mpadded',
+    'mphantom',
+    'mroot',
+    'mrow',
+    'msqrt',
+    'mstyle',
+    'mmultiscripts',
+    'mover',
+    'mprescripts',
+    'msub',
+    'msubsup',
+    'msup',
+    'munder',
+    'munderover',
+    'none',
+    'maligngroup',
+    'malignmark',
+    'mtable',
+    'mtd',
+    'mtr',
+    'mlongdiv',
+    'mscarries',
+    'mscarry',
+    'msgroup',
+    'msline',
+    'msrow',
+    'mstack',
+    'maction',
+    'semantics',
+    'annotation',
+    'annotation-xml'
+];
 export default {
+    markdown: {
+        config: (md) => {
+            md.use(markdownItKatex)
+        }
+    },
+    vue: {
+        template: {
+            compilerOptions: {
+                isCustomElement: (tag) => customElements.includes(tag)
+            }
+        }
+    },
     base: "/",
     title: "ProDocs",
     description: "一站式编程学习网站",
@@ -9,6 +110,11 @@ export default {
             'link', {rel: 'icon', href: '/favicon.ico'}
         ],
         ['meta', {name: 'baidu-site-verification', content: 'codeva-UJHMQmYe6P'}],
+        ['link', {
+            rel: 'stylesheet',
+            href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css',
+            crossorigin: ''
+        }]
     ],
     themeConfig: {
         search: {
